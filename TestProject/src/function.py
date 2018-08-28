953123b4f538b1f7b889fa0607b375c0136fde0c
jaspe <jaspe@145.108.76.27>
2018-08-28 18:07:57 +0200

    New Module for trying out functions

---
+++ TestProject/src/function.py
@@ -0,0 +1,50 @@
+'''
+C:/Users/jaspe/Documents/School/VU/Python Programming/VU/TestProject/src/function.py
+Purpose:
+    ...
+
+Version:
+    1            First Starts
+    
+Date:
+    28 Aug 2018
+    
+@author: jbn601
+'''
+########################################################
+### Imports
+# import numpy as np
+# import scipy as sp
+# import matplotlib.pyplot as plt
+# from gurobi import *
+
+#######################################################
+### printStr
+def printStr(sIn):
+    '''
+    Purpose:    
+        Prints the given argument.
+    Inputs:
+        sIn        String, argument to print
+    Return value:
+    '''
+    # Magic numbers
+    
+    # Initialisation
+    
+    # Output
+    print(sIn)
+#######################################################
+### Main
+def main():
+    # Magic numbers
+    sA = "Getal"
+    # Initialisation
+    
+    # Output
+    printStr(sA)
+    
+####################################################
+### start main
+if __name__ == '__main__':
+    main()
